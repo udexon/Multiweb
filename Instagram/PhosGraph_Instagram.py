ef56dcf61c0b4b850909b56b83556a486408660e
udexon <udexon@users.noreply.github.com>
2020-07-25 21:10:57 +0800

    Create PhosGraph_Instagram.py

---
+++ Instagram/PhosGraph_Instagram.py
@@ -0,0 +1,21 @@
+# open PhosGraph POST page in new tab
+windows_before = driver.current_window_handle
+tab_ig = windows_before
+driver.execute_script("window.open('http://phos.epizy.com/phos/post_rc.php?r=&c=')")
+windows_after = driver.window_handles
+tab_phos = [x for x in windows_after if x != windows_before][0]
+driver.switch_to.window(tab_phos)
+
+# enter dictionary j as json
+import json
+u_j=driver.find_element_by_xpath('//*[@id="json"]')
+u_j.send_keys(json.dumps(j))
+u_f=driver.find_element_by_name("form_json")
+u_f.submit()
+
+# HTTP GET extract number of followers from PhosGraph
+import base64
+import urllib
+s1='https://www.instagram.com/jessicachastain/'
+driver.get("http://phos.epizy.com/phos/get.php?r=data/log_fi:_c_get:_d64:_g:_av:_cx:_1_-_i:_pjs:_0_i:_jd:_followers_i:_ON_ECHO_bv:_ec:&c="+urllib.parse.quote(base64.b64encode(s1.encode())))
+t_followers=driver.find_element_by_tag_name('body').get_attribute('innerHTML')
